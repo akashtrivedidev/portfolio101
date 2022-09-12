e0d6553b3bc95c8b1ea0aee3f6ed50ecd3718eed
Akash-Trivedi <sunnytrivedi321@gmail.com>
2022-09-13 01:52:09 +0530

    social media added, empty data ui added

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,14 +3,29 @@ import React from 'react'
 
 const ProjectCard = (props) => {
   const { name, description, thumbnail, topics } = props.project
+  const { hasdata } = props
+  let project_name = hasdata ? name.replace(/-/, ' ') : ''
+  project_name = `${project_name.charAt(0).toUpperCase()}${project_name.slice(1,)}`
 
+  if (!hasdata) {
+    return (
+      <div className='project-card-container'>
+        <div className="project-thumbnail-container">
+          <div className="empty-project-thumbnail"></div>
+        </div>
+        <div className="projection-about-container">
+          <div className="empty-line"></div>
+        </div>
+      </div>
+    )
+  }
   return (
     <div className='project-card-container'>
       <div className="project-thumbnail-container">
         <img src={thumbnail} alt="" className="project-thumbnail" />
       </div>
       <div className="projection-about-container">
-        <h3 className="project-name">{name}</h3>
+        <h3 className="project-name">{project_name}</h3>
         <hr />
         <div className="project-description">{description}</div>
         <div className="project-topic-container">
